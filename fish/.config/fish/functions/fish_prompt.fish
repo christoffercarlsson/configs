a7368bdd635809b311e5b39b83a95ba6ed4aa072
Christoffer Carlsson <cc@christoffercarlsson.se>
2025-04-01 08:49:50 +0200

    Update Fish prompt to show Git status

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,8 @@
 function fish_prompt
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showuntrackedfiles true
+    set -g __fish_git_prompt_showstashstate true
     echo ''
-    string join '' -- (set_color blue)$PWD (set_color yellow)(fish_vcs_prompt)
+    string join '' -- (set_color blue)(prompt_pwd --full-length-dirs=3) (set_color yellow)(fish_git_prompt)
     echo (set_color normal)'> '
 end
