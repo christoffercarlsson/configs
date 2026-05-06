616085bb321138c07f1518936a7d7ef87e4c4018
Christoffer Carlsson <cc@christoffercarlsson.com>
2026-05-06 15:16:39 +0200

    Update Fish configuration

--- fish/.config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-switch (uname)
-case Linux
-    fish_add_path /usr/local/go/bin $HOME/go/bin
-    set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/tkey-ssh-agent/sock
-case Darwin
-    fish_add_path /opt/homebrew/bin
-    set -x SSH_AUTH_SOCK /opt/homebrew/var/run/tkey-ssh-agent.sock
-end
